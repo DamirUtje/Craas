32305ecad5b83cfb22f773fde049cfd7a472f7b8
damir <d4mirispg@hotmail.de>
2018-11-03 22:27:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# craas
+NORDAKADEMIE Master Project
---
+++ src/main/java/de/nordakademie/craas/CraasApplication.java
@@ -0,0 +1,12 @@
+package de.nordakademie.craas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraasApplication.class, args);
+	}
+}
