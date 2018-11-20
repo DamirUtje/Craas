bdcf7075bfaebc2cb6d04a60934f193b30e27540
FrankBracker <44726590+FrankBracker@users.noreply.github.com>
2018-11-20 19:36:55 +0100

    added possibilities to start h2: currently starting as Server
    h2 db is started as Server im main method in java CraasApplication.java
    second choice: starting h2 as embedded db

--- src/main/java/de/nordakademie/craas/CraasApplication.java
+++ src/main/java/de/nordakademie/craas/CraasApplication.java
@@ -1,13 +1,43 @@
 package de.nordakademie.craas;
 
 
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class CraasApplication {
+public class CraasApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		startH2Server();
 		SpringApplication.run(CraasApplication.class, args);
+		
+//		// start the TCP Server
+//		Server server = Server.createTcpServer(args).start();
 	}
+	
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        startH2Server();
+	        return application.sources(CraasApplication.class);
+	    }
+
+	    private static void startH2Server() {
+	        try {
+	            Server h2Server = Server.createTcpServer().start();
+	            if (h2Server.isRunning(true)) {
+	                //log.info("H2 server was started and is running.");
+	            	System.out.println("H2 server was started and is running.");
+	            } else {
+	                throw new RuntimeException("Could not start H2 server.");
+	            }
+	        } catch (SQLException e) {
+	            throw new RuntimeException("Failed to start H2 server: ", e);
+	        }
+	    }
+	
 }
