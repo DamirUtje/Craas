0afdecd9de1d6425ad60cdc10e3b5bb11176bf95
FrankBracker <44726590+FrankBracker@users.noreply.github.com>
2018-11-21 21:17:01 +0100

    server config + data.sql + schema.sql moved to CraasDataService

--- src/main/java/de/nordakademie/craas/CraasApplication.java
+++ src/main/java/de/nordakademie/craas/CraasApplication.java
@@ -14,30 +14,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class CraasApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		startH2Server();
+
 		SpringApplication.run(CraasApplication.class, args);
 		
 //		// start the TCP Server
 //		Server server = Server.createTcpServer(args).start();
 	}
-	
-	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	        startH2Server();
-	        return application.sources(CraasApplication.class);
-	    }
 
-	    private static void startH2Server() {
-	        try {
-	            Server h2Server = Server.createTcpServer().start();
-	            if (h2Server.isRunning(true)) {
-	                //log.info("H2 server was started and is running.");
-	            	System.out.println("H2 server was started and is running.");
-	            } else {
-	                throw new RuntimeException("Could not start H2 server.");
-	            }
-	        } catch (SQLException e) {
-	            throw new RuntimeException("Failed to start H2 server: ", e);
-	        }
-	    }
+
 	
 }
