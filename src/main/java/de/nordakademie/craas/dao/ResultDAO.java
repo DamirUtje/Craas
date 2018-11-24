46b1eaae36221313a31c5fd9de4fb14087629e19
damir <d4mirispg@hotmail.de>
2018-11-24 18:07:28 +0100

    Fixed style; Implement data loading from db via rest api;

--- src/main/java/de/nordakademie/craas/dao/ResultDAO.java
+++ src/main/java/de/nordakademie/craas/dao/ResultDAO.java
@@ -3,6 +3,7 @@ package de.nordakademie.craas.dao;
 import de.nordakademie.craas.model.Result;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,12 +12,40 @@ public class ResultDAO {
 
 
     public List<Result> loadDataByTerm(String term) {
+
+        List<Result> results = new ArrayList<>();
+
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:h2:/home/damir/test.h2.db", "", "");
+            PreparedStatement statement = conn.prepareStatement("SELECT * FROM UN_SL_INDIVIDUAL");
+            ResultSet resultSet = statement.executeQuery();
+
+
+            if (resultSet != null) {
+                while (resultSet.next()) {
+
+                    Result result = new Result(resultSet.getInt("DATAID"),
+                            resultSet.getString("FIRST_NAME"),
+                            resultSet.getString("SECOND_NAME"));
+
+                    results.add(result);
+                }
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+
+        return results;
+    }
+
+    private List<Result> getFakeData(String term){
         List<Result> results = new ArrayList<>();
 
         results.add(new Result(1, term, term));
         results.add(new Result(1, term + 1, term + 1));
         results.add(new Result(1, term + 2, term + 2));
-
         return results;
     }
 }
