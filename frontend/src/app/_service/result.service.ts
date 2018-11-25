091eb3600bae30e2fde4ce76f162f86f7dd4cfde
damir <d4mirispg@hotmail.de>
2018-11-25 19:54:08 +0100

    code formatting :)

--- frontend/src/app/_service/result.service.ts
+++ frontend/src/app/_service/result.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Router } from "@angular/router";
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Router} from "@angular/router";
 
-import { Observable, of, throwError } from "rxjs"
-import { map, switchMap, catchError } from "rxjs/operators"
+import {Observable, of, throwError} from "rxjs"
+import {map, switchMap, catchError} from "rxjs/operators"
 
-import { Result } from '../_model';
+import {Result} from '../_model';
 
 @Injectable()
 export class ResultService {
@@ -18,12 +18,12 @@ export class ResultService {
   // TODO: Documentation => https://juristr.com/blog/2016/11/configure-proxy-api-angular-cli/
 
   loadResults(term: string): Observable<Result[]> {
-    const options = { params: new HttpParams().set('term', term) };
+    const options = {params: new HttpParams().set('term', term)};
     return this.http.get<Result[]>(this.baseApiUrl + 'query', options);
   }
 
   loadSuggestions(term: string): Observable<Result[]> {
-    const options = { params: new HttpParams().set('term', term) };
+    const options = {params: new HttpParams().set('term', term)};
     return this.http.get<Result[]>(this.baseApiUrl + 'suggest', options);
   }
 
