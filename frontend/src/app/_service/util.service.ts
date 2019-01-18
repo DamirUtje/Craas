b4d6d021938b512fa3538531500ca5413f5dd078
damir <d4mirispg@hotmail.de>
2019-01-18 23:08:13 +0100

    Fix mobile support;

--- frontend/src/app/_service/util.service.ts
+++ frontend/src/app/_service/util.service.ts
@@ -15,9 +15,16 @@ export class UtilService {
   }
 
   isMobile(): boolean {
-    if(window.innerWidth <= 800 && window.innerHeight <= 600) {
+    if(navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i)){
       return true;
-    } else {
+    }
+    else {
       return false;
     }
   }
