b4b4c6164e5b1d79d2fefc84aecd5680a04806bb
FrankBracker <44726590+FrankBracker@users.noreply.github.com>
2019-02-04 23:19:53 +0100

    IndexController and RenewIndex Service implemented

---
+++ src/main/java/de/nordakademie/craas/service/RenewIndexService.java
@@ -0,0 +1,10 @@
+package de.nordakademie.craas.service;
+
+/**
+ * Service for the Result entity.
+ * @author Frank, Damir
+ *
+ */
+public interface RenewIndexService {
+    void RenewIndex();
+}
---
+++ src/main/java/de/nordakademie/craas/service/RenewIndexServiceImpl.java
@@ -0,0 +1,18 @@
+package de.nordakademie.craas.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Service impl for the Application restart.
+ * @author Frank, Damir
+ *
+ */
+@Component
+public class RenewIndexServiceImpl implements RenewIndexService {
+
+	@Override
+	public void RenewIndex() {
+		// TODO Auto-generated method stub
+		
+	}
+}
