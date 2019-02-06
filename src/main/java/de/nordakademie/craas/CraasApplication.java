f33bc08bb400e602b09defe2ca7199e986a1bfa5
damir <d4mirispg@hotmail.de>
2019-02-06 20:51:00 +0100

    Imports

--- src/main/java/de/nordakademie/craas/CraasApplication.java
+++ src/main/java/de/nordakademie/craas/CraasApplication.java
@@ -1,18 +1,12 @@
 package de.nordakademie.craas;
 
 import de.nordakademie.craas.service.RenewIndexService;
-import org.hibernate.search.jpa.FullTextEntityManager;
-import org.hibernate.search.jpa.Search;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationListener;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 /**
  * Application
